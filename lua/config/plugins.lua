53f64e2014c311fe9412b6c21a41c05d79fde587
Martin Turoci <martin.turoci@h2o.ai>
2024-01-02 10:04:05 +0100

    feat: Add theme and fix commenting.

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,4 +1,13 @@
 return {
-    {'numToStr/Comment.nvim', opts = {}, lazy = false },
-    { "williamboman/mason.nvim"}
+    { 'numToStr/Comment.nvim', opts = {}, lazy = false },
+    { "williamboman/mason.nvim"},
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.cmd([[colorscheme tokyonight-storm]])
+        -- require("config.colorscheme")
+      end,
+    },
 }
