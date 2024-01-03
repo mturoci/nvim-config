bebda366319cce938221fe5705aae7a814bc9f31
Martin Turoci <martin.turoci@h2o.ai>
2024-01-03 10:27:11 +0100

    feat: Add surround plugin.

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -17,6 +17,16 @@ return {
         end
     },
     {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+    {
       "folke/tokyonight.nvim",
       lazy = false,
       priority = 1000,
