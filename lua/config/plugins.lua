c942c472834ba8e8502eae902a252dd35ba4b34a
Martin Turoci <martin.turoci@h2o.ai>
2024-01-03 11:04:21 +0100

    feat: Add Gitsigns plugin.

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -8,6 +8,15 @@ return {
     },
     { 'nvim-tree/nvim-web-devicons'},
     { 
+      'lewis6991/gitsigns.nvim',
+      config = function ()
+        require('gitsigns').setup {
+          current_line_blame = true,
+          current_line_blame_formatter = '<author>, <author_time:%R> • <summary>',
+        }
+      end
+    },
+    { 
       'nvim-telescope/telescope.nvim',
       tag = '0.1.5',
       dependencies = { 'nvim-lua/plenary.nvim' },
