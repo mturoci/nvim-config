5956838f3b7ebfa6e40af176d5c60a33305bc173
Martin Turoci <martin.turoci@h2o.ai>
2024-01-03 10:16:42 +0100

    feat: Add Telescope plugin.

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+local keymap = vim.keymap.set
+local silent = { silent = true }
+local builtin = require('telescope.builtin')
+
+-- Telescope find/grep files
+keymap('n', '<leader>p', builtin.find_files, {})
+keymap('n', '<leader>fg', builtin.live_grep, {})
+keymap('n', '<leader>g', builtin.git_status, {})
+
+-- Window management.
+keymap("n", "<C-h>", "<C-w>h", silent)
+keymap("n", "<C-j>", "<C-w>j", silent)
+keymap("n", "<C-k>", "<C-w>k", silent)
+keymap("n", "<C-l>", "<C-w>l", silent)
+keymap("n", "<C-w>", "<C-w>w", silent)
--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,6 +1,21 @@
 return {
     { 'numToStr/Comment.nvim', opts = {}, lazy = false },
     { "williamboman/mason.nvim"},
+    { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }, lazy = false },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function () 
+          local configs = require("nvim-treesitter.configs")
+
+          configs.setup({
+              ensure_installed = { "lua", "vim", "vimdoc", "css", "python", "go", "javascript", "typescript", "html", "svelte", "json", "markdown" },
+              sync_install = false,
+              highlight = { enable = true },
+              indent = { enable = true },  
+            })
+        end
+    },
     {
       "folke/tokyonight.nvim",
       lazy = false,
