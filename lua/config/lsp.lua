0ccce91403c49a7181728611588e2333233c46cd
Martin Turoci <martin.turoci@h2o.ai>
2024-01-05 17:27:57 +0100

    feat: Add Lua LSP autocomplete

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -3,12 +3,16 @@ require("mason-lspconfig").setup({
   ensure_installed = { "lua_lsp", "tsserver", "gopls", "pyright" },
   automatic_installation = true
 })
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("mason-lspconfig").setup_handlers {
   function(server_name)
-    require("lspconfig")[server_name].setup {}
+    require("lspconfig")[server_name].setup {
+      capabilities = capabilities
+    }
   end,
   ["lua_ls"] = function()
     require("lspconfig").lua_ls.setup({
+      capabilities = capabilities,
       settings = {
         Lua = {
           diagnostics = {
--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -159,9 +159,11 @@ return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
-      "hrsh7th/cmp-buffer",           -- source for text in buffer
-      "hrsh7th/cmp-path",             -- source for file system paths
-      "L3MON4D3/LuaSnip",             -- snippet engine
+      'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
+      "hrsh7th/cmp-buffer",   -- source for text in buffer
+      "hrsh7th/cmp-path",     -- source for file system paths
+      "L3MON4D3/LuaSnip",
+      -- snippet engine
       "saadparwaiz1/cmp_luasnip",     -- for autocompletion
       "rafamadriz/friendly-snippets", -- useful snippets
       "onsails/lspkind.nvim",         -- vs-code like pictograms
@@ -206,5 +208,5 @@ return {
         },
       })
     end,
-  }
+  },
 }
