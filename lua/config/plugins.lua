59f2d07206c590a64540c85080b9d6a6d941783d
Martin Turoci <martin.turoci@h2o.ai>
2024-01-05 10:41:47 +0100

    feat: File finder results formatting.

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -80,15 +80,31 @@ return {
     lazy = false,
     config = function()
       local actions = require('telescope.actions')
+      local function filenameFirst(_, path)
+        local tail = vim.fs.basename(path)
+        local parent = vim.fs.dirname(path)
+        if parent == "." then return tail end
+        return string.format("%s\t\t%s", tail, parent)
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "TelescopeResults",
+        callback = function(ctx)
+          vim.api.nvim_buf_call(ctx.buf, function()
+            vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+            vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+          end)
+        end,
+      })
+
       require("telescope").setup({
+        pickers = {
+          find_files = { path_display = filenameFirst, }
+        },
         defaults = {
           mappings = {
-            n = {
-              ["<S-CR>"] = actions.select_vertical,
-            },
-            i = {
-              ["<S-CR>"] = actions.select_vertical,
-            }
+            n = { ["<S-CR>"] = actions.select_vertical, },
+            i = { ["<S-CR>"] = actions.select_vertical, }
           }
         }
       })
