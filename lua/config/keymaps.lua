f0fe34a26fddffdbab8c6d49c958de6a5c062869
Martin Turoci <martin.turoci@h2o.ai>
2024-01-05 15:36:10 +0100

    feat: Show help in a new buffer.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -43,3 +43,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, opts)
   end,
 })
+
+-- Show help in a new buffer.
+
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  pattern = '*',
+  callback = function(event)
+    if vim.bo[event.buf].filetype == 'help' then vim.cmd.only() end
+  end,
+})
