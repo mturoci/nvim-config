707efce5518a863fcdf329328a96a41c4162eae7
Martin Turoci <martin.turoci@h2o.ai>
2024-01-05 21:09:51 +0100

    feat: Show only icons in autocomplete.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,9 +1,10 @@
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = { "lua_lsp", "tsserver", "gopls", "pyright" },
   automatic_installation = true
 })
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("mason-lspconfig").setup_handlers {
   function(server_name)
     require("lspconfig")[server_name].setup {
