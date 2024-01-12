e7039bca6f1ab61d197c24476aa758025253978f
Martin Turoci <martin.turoci@h2o.ai>
2024-01-12 19:51:08 +0100

    feat: Add vim-illuminate.

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -10,6 +10,27 @@ return {
   { 'nvim-tree/nvim-web-devicons' },
   { 'github/copilot.vim' },
   {
+    'RRethy/vim-illuminate',
+    config = function()
+      require("illuminate").configure {}
+
+      -- change the highlight style
+      vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+      vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+      vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+
+      --- auto update the highlight style on colorscheme change
+      vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+        pattern = { "*" },
+        callback = function(ev)
+          vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+          vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+          vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+        end
+      })
+    end
+  },
+  {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
