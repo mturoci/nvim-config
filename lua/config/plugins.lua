0dc3bfca3d02b04e6f0c92e36647b0e2718525fd
Martin Turoci <martin.turoci@h2o.ai>
2024-01-13 19:30:32 +0100

    feat: Add vim abolish plugin

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -9,6 +9,7 @@ return {
   { 'neovim/nvim-lspconfig' },
   { 'nvim-tree/nvim-web-devicons' },
   { 'github/copilot.vim' },
+  { 'tpope/vim-abolish' },
   {
     'RRethy/vim-illuminate',
     config = function()
