883b9bd0a8deb7b3f4115e8d721237752b585148
Martin Turoci <martin.turoci@h2o.ai>
2024-01-19 08:28:34 +0100

    feat: Statusline - colors.

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -1,12 +1,23 @@
 local powerline_right = " %#Background#"
 local powerline_left = "%#Background#%#BackgroundLight# "
-local bg = "#262626"
-local fg = "#444444"
-local primary = "#a7c080"
+local COLOR_BG = "#262626"
+local COLOR_FG = "#444444"
+local COLOR_PRIMARY = "#a7c080"
+local COLOR_ERR = vim.api.nvim_get_hl(0, { name = "DiagnosticError" }).fg
+local COLOR_WARN = vim.api.nvim_get_hl(0, { name = "DiagnosticWarn" }).fg
+local COLOR_HINT = vim.api.nvim_get_hl(0, { name = "DiagnosticHint" }).fg
+local COLOR_INFO = vim.api.nvim_get_hl(0, { name = "DiagnosticInfo" }).fg
+
+vim.cmd(table.concat({ "highlight Background guibg=", COLOR_BG, " guifg=", COLOR_FG }))
+vim.cmd(table.concat({ "highlight BackgroundLight guibg=", COLOR_FG, " guifg=", COLOR_PRIMARY }))
+vim.cmd(table.concat({ "highlight StatusLineError guibg=", COLOR_FG, " guifg=#", ("%06x"):format(COLOR_ERR) }))
+vim.cmd(table.concat({ "highlight StatusLineWarn guibg=", COLOR_FG, " guifg=#", ("%06x"):format(COLOR_WARN) }))
+vim.cmd(table.concat({ "highlight StatusLineHint guibg=", COLOR_FG, " guifg=#", ("%06x"):format(COLOR_HINT) }))
+vim.cmd(table.concat({ "highlight StatusLineInfo guibg=", COLOR_FG, " guifg=#", ("%06x"):format(COLOR_INFO) }))
 
 local function git_info()
-  local staged = 0
-  local changed = 0
+  local staged = 1
+  local changed = 1
   local branch = 'main'
   -- TODO: Make this more performant.
   -- local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD"):gsub("\n", "")
@@ -16,7 +27,11 @@ local function git_info()
   -- local unpulled = vim.fn.system("git log ..@{u}"):gsub("\n", "")
   local unpushed = -1
   local unpulled = -1
-  return branch, staged, changed, unpushed, unpulled
+
+  local stagedStr = staged > 0 and table.concat({ "%#StatusLineInfo#", "  ", staged, "%#BackgroundLight#" }) or ""
+  local changedStr = changed > 0 and table.concat({ "%#StatusLineWarn#", " 󰏫 ", changed, "%#BackgroundLight#" }) or ""
+
+  return branch, stagedStr, changedStr, unpushed, unpulled
 end
 local webdevicons = require 'nvim-web-devicons'
 
@@ -34,7 +49,14 @@ local function lsp_info()
   local warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
   local hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
   local info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
-  return errors, warnings, hints, info
+
+  local errorsStr = errors > 0 and table.concat({ "%#StatusLineError#", "  ", errors, "%#BackgroundLight#" }) or ""
+  local warningsStr = warnings > 0 and table.concat({ "%#StatusLineWarn#", "  ", warnings, "%#BackgroundLight# " }) or
+      ""
+  local hintsStr = hints > 0 and table.concat({ "%#StatusLineHint#", "  ", hints, "%#BackgroundLight# " }) or ""
+  local infoStr = info > 0 and table.concat({ "%#StatusLineInfo#", "  ", info, "%#BackgroundLight#" }) or ""
+
+  return errorsStr, warningsStr, hintsStr, infoStr
 end
 
 local function cursor_info()
@@ -45,7 +67,7 @@ end
 local function get_left()
   local branch, staged, changed, unpushed, unpulled = git_info()
   return table.concat({
-    "  ", branch, "  ", staged, "  ", changed, "  ", unpushed, "  ", unpulled, powerline_right
+    "  ", branch, staged, changed, "  ", unpushed, "  ", unpulled, powerline_right
   })
 end
 
@@ -54,14 +76,12 @@ local function get_center()
   local errors, warnings, hints, info = lsp_info()
 
   if icon then
-    vim.cmd(table.concat({ "highlight BackgroundIcon guibg=", fg, " guifg=", color }))
+    vim.cmd(table.concat({ "highlight BackgroundIcon guibg=", COLOR_FG, " guifg=", color }))
   end
 
   return table.concat({
-    powerline_left, "%#BackgroundIcon#", icon or "", "%#BackgroundLight# ", filename, dirty == 1 and "*" or " ", "  ",
-    errors,
-    "  ", warnings, "  ", hints,
-    "  ", info,
+    powerline_left, "%#BackgroundIcon#", icon or "", "%#BackgroundLight# ", filename, dirty == 1 and "*" or " ",
+    errors, warnings, hints, info,
     powerline_right
   })
 end
@@ -119,6 +139,4 @@ end
 -- _G.statusline = debounce(get_statusline, 1000)
 _G.statusline = get_statusline
 
-vim.cmd(table.concat({ "highlight Background guibg=", bg, " guifg=", fg }))
-vim.cmd(table.concat({ "highlight BackgroundLight guibg=", fg, " guifg=", primary }))
 vim.o.statusline = "%!v:lua.statusline()"
