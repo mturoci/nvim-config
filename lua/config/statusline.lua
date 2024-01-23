76d200b5c3f57f141ad792555a64abb9e668c312
Martin Turoci <martin.turoci@h2o.ai>
2024-01-23 16:59:30 +0100

    feat: Move git status statusline updates into a background job.

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -91,7 +91,7 @@ local function git_info()
       local result = j:result()
       if result == nil then return end
 
-      for line in j:result()[1]:gmatch("[^\r\n]+") do
+      for line in result:gmatch("[^\r\n]+") do
         if string.sub(line, 1, 2) == "??" then untracked = untracked + 100 end
         if string.sub(line, 1, 1) ~= " " then staged = staged + 1 end
         if string.sub(line, 2, 2) ~= " " then changed = changed + 1 end
