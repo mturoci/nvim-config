69fbe3853f222749bfba37df79131ea19922b7c4
Martin Turoci <martin.turoci@h2o.ai>
2024-02-16 13:43:32 +0100

    feat: Center ctrl O and ctrl I.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -52,6 +52,8 @@ keymap('n', '<leader>w', ':write<CR>', silent)
 keymap('n', '<leader>q', ':q!<CR>', silent)
 keymap('n', '<C-u>', '<C-u>zz', silent)
 keymap('n', '<C-d>', '<C-d>zz', silent)
+keymap('n', '<C-o>', '<C-o>zz', silent)
+keymap('n', '<C-i>', '<C-i>zz', silent)
 
 -- LSP.
 vim.api.nvim_create_autocmd('LspAttach', {
