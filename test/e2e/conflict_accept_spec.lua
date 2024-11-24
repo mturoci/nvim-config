396db73c53933740cf89e3b4dd9466bbfa0754c7
Martin Turoci <martin.turoci@h2o.ai>
2024-11-24 21:10:12 +0100

    test: Check if accept is ignored if not intersecting with a conflict

--- test/e2e/conflict_accept_spec.lua
+++ test/e2e/conflict_accept_spec.lua
@@ -1,6 +1,6 @@
 local jobopts = { rpc = true, width = 80, height = 24, env = { NVIM_ENV = 'test' } }
 local eq = assert.is.equal
-local original_file_content = ''
+local original_file_content = {}
 local fixture_file = './test/fixtures/conflict_other.txt'
 
 describe('Conflict accept', function()
@@ -19,7 +19,7 @@ describe('Conflict accept', function()
   end)
 
 
-  it('Accepts right conflict and updates all buffers properly #run', function()
+  it('Accepts right conflict and updates all buffers properly', function()
     vim.fn.rpcrequest(nvim, 'nvim_command', 'edit ' .. fixture_file)
     vim.fn.rpcrequest(nvim, 'nvim_input', 'j')
 
@@ -39,7 +39,7 @@ describe('Conflict accept', function()
     eq(expected, table.concat(result, '\n'))
   end)
 
-  it('Accepts left conflict and updates all buffers properly #run', function()
+  it('Accepts left conflict and updates all buffers properly', function()
     vim.fn.rpcrequest(nvim, 'nvim_command', 'edit ' .. fixture_file)
     vim.fn.rpcrequest(nvim, 'nvim_input', '<C-W>w')
     vim.fn.rpcrequest(nvim, 'nvim_input', 'j')
@@ -58,5 +58,23 @@ describe('Conflict accept', function()
     eq(expected, table.concat(result, '\n'))
     result = vim.fn.rpcrequest(nvim, 'nvim_buf_get_lines', 0, 0, -1, false)
     eq(expected, table.concat(result, '\n'))
+  end)
+
+  it('Does not do anything if cursor not at conflict #run', function()
+    vim.fn.rpcrequest(nvim, 'nvim_command', 'edit ' .. fixture_file)
+
+    local leader_key = vim.fn.rpcrequest(nvim, 'nvim_eval', 'mapleader')
+    vim.fn.rpcrequest(nvim, 'nvim_feedkeys', vim.api.nvim_replace_termcodes(leader_key .. 'a', true, false, true), 'm',
+      true)
+
+    -- Add sleep to wait for the command to finish.
+    vim.fn.rpcrequest(nvim, 'nvim_command', 'sleep 100m')
+
+    local result = vim.fn.rpcrequest(nvim, 'nvim_buf_get_lines', 2, 0, -1, false)
+    eq('Regular text.\nOurs conflict.\nRegular text.', table.concat(result, '\n'))
+    result = vim.fn.rpcrequest(nvim, 'nvim_buf_get_lines', 1, 0, -1, false)
+    eq(table.concat(original_file_content, '\n'), table.concat(result, '\n'))
+    result = vim.fn.rpcrequest(nvim, 'nvim_buf_get_lines', 0, 0, -1, false)
+    eq('Regular text.\nTheirs conflict.\nRegular text.', table.concat(result, '\n'))
   end)
 end)
