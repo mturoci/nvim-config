83c550afa04127189bf591d01fc7b55eacc3eb57
Martin Turoci <martin.turoci@h2o.ai>
2025-04-03 21:42:27 +0200

    test: Add a buffer navigation test.

---
+++ test/e2e/conflict_navigation_spec.lua
@@ -0,0 +1,26 @@
+local jobopts = { rpc = true, width = 80, height = 24, env = { NVIM_ENV = 'test' } }
+local eq = assert.is.equal
+
+describe('Conflict navigation', function()
+  local nvim
+
+  -- TODO: Do not spawn a new process for each test. Closing should be enough.
+  before_each(function()
+    nvim = vim.fn.jobstart({ 'nvim', '--embed', '--headless' }, jobopts)
+  end)
+
+  after_each(function()
+    print(vim.fn.rpcrequest(nvim, 'nvim_eval', "execute('messages')"))
+    vim.fn.jobstop(nvim)
+  end)
+
+
+  it('Do not jump to original buffer when using CTRL-W w #run', function()
+    vim.fn.rpcrequest(nvim, 'nvim_command', 'edit ./test/fixtures/conflict_other.txt')
+    eq(3, vim.fn.rpcrequest(nvim, 'nvim_eval', 'bufnr("%")'))
+    vim.fn.rpcrequest(nvim, 'nvim_input', '<C-W>w')
+    eq(2, vim.fn.rpcrequest(nvim, 'nvim_eval', 'bufnr("%")'))
+    vim.fn.rpcrequest(nvim, 'nvim_input', '<C-W>w')
+    eq(3, vim.fn.rpcrequest(nvim, 'nvim_eval', 'bufnr("%")'))
+  end)
+end)
