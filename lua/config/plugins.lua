d84777543b1a4d3745047bbde49548d5d929f109
Martin Turoci <martin.turoci@h2o.ai>
2025-07-14 13:24:22 +0200

    feat: Cleanup autocomplete.

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -281,9 +281,7 @@ return {
         -- sources for autocompletion
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" }, -- snippets
-          { name = "buffer" },  -- text within current buffer
-          { name = "path" },    -- file system paths
+          { name = "path" }, -- file system paths
         }),
         -- configure lspkind for vs-code like pictograms in completion menu
         formatting = {
